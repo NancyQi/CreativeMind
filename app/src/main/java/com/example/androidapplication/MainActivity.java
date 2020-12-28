fcd975e40f1d456e45acf65a4073ac2e8e60346e
NancyQi <2456885100@qq.com>
2020-12-28 14:46:41 +0800

    拍照代码

--- app/src/main/java/com/example/androidapplication/MainActivity.java
+++ app/src/main/java/com/example/androidapplication/MainActivity.java
@@ -1,14 +1,60 @@
 package com.example.androidapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Locale;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+@SuppressLint("SdCardPath")
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    private Button button;
+    private ImageView view;
+    private int TAKE_PHOTOS_CODE=200;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button1);
+        view = (ImageView) findViewById(R.id.imageView1);
+        button.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, TAKE_PHOTOS_CODE);
+            }
+        });
     }
+
+    @SuppressLint("SdCardPath")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        if (requestCode == TAKE_PHOTOS_CODE && resultCode == RESULT_OK) {
+            Bitmap bm = (Bitmap) data.getExtras().get("data");
+            view.setImageBitmap(bm);
+        }
+    }
+
 }
