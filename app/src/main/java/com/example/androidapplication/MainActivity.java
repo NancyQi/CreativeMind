ae57850dd992be5c8b8cecc3bc48d64c5bff4381
NancyQi <2456885100@qq.com>
2021-01-07 13:42:43 +0800

    增加的两个页面,要在manifest.xml里添加新增的两个页面

--- app/src/main/java/com/example/androidapplication/MainActivity.java
+++ app/src/main/java/com/example/androidapplication/MainActivity.java
@@ -1,60 +1,44 @@
 package com.example.androidapplication;
 
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Calendar;
-import java.util.Locale;
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
-import android.text.format.DateFormat;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
 
-@SuppressLint("SdCardPath")
+
 public class MainActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
-    private Button button;
-    private ImageView view;
-    private int TAKE_PHOTOS_CODE=200;
+    private Button callbutton;
+    private Button recbutton;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        button = (Button) findViewById(R.id.button1);
-        view = (ImageView) findViewById(R.id.imageView1);
-        button.setOnClickListener(new OnClickListener() {
+        setContentView(R.layout.main);
+        callbutton = (Button) findViewById(R.id.callbutton);
+        recbutton = (Button) findViewById(R.id.recbutton);
+
+        callbutton.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, TAKE_PHOTOS_CODE);
+
+                Intent intent = new Intent(MainActivity.this,Calling.class);
+                startActivity(intent);
+
+            }
+        });
+
+        recbutton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this,Recognize.class);
+                startActivity(intent);
             }
         });
     }
-
-    @SuppressLint("SdCardPath")
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // TODO Auto-generated method stub
-        if (requestCode == TAKE_PHOTOS_CODE && resultCode == RESULT_OK) {
-            Bitmap bm = (Bitmap) data.getExtras().get("data");
-            view.setImageBitmap(bm);
-        }
-    }
-
 }
--- app/src/main/java/com/example/androidapplication/Recognize.java
+++ app/src/main/java/com/example/androidapplication/Recognize.java
@@ -1,13 +1,21 @@
 package com.example.androidapplication;
-import android.app.Activity;
-import android.content.Context;
-import android.util.Log;
 
-import com.aiunit.common.protocol.gesture.GestureLandmark;
+import com.aiunit.common.protocol.types.Rect2Pt;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+
 import com.aiunit.common.protocol.gesture.GestureResult;
 import com.aiunit.common.protocol.gesture.GestureResultList;
-import com.aiunit.common.protocol.gesture.GestureType;
-import com.aiunit.common.protocol.types.Rect2Pt;
 import com.aiunit.vision.common.ConnectionCallback;
 import com.aiunit.vision.gesture.HandInputSlot;
 import com.aiunit.vision.gesture.HandOutputSlot;
@@ -17,54 +25,108 @@ import com.coloros.ocs.base.common.ConnectionResult;
 import com.coloros.ocs.base.common.api.OnConnectionFailedListener;
 import com.coloros.ocs.base.common.api.OnConnectionSucceedListener;
 
-import java.util.List;
-
+@SuppressLint("SdCardPath")
 public class Recognize extends Activity {
-    CVUnitClient mCVClient;
+    private ImageView view;
+    private final int TAKE_PHOTOS_CODE = 200;
 
-    public void initial(){
-        mCVClient = CVUnit.getGestureLandmarkDetectorClient
-                (this.getApplicationContext()).addOnConnectionSucceedListener(new OnConnectionSucceedListener() {
-            @Override
-            public void onConnectionSucceed() {
-                Log.i("TAG", " authorize connect: onConnectionSucceed");
-            }
-        }).addOnConnectionFailedListener(new OnConnectionFailedListener() {
-            @Override
-            public void onConnectionFailed(ConnectionResult connectionResult) {
-                Log.e("TAG", " authorize connect: onFailure: " + connectionResult.getErrorCode());
-            }
-        });
-        mCVClient.initService(this, new ConnectionCallback() {
-            @Override
-            public void onServiceConnect() {
-                Log.i("TAG", "initService: onServiceConnect");
-                int startCode = mCVClient.start();
-            }
-
-            @Override
-            public void onServiceDisconnect() {
-                Log.e("TAG", "initService: onServiceDisconnect: ");
-            }
-        });
-        HandInputSlot inputSlot = (HandInputSlot) mCVClient.createInputSlot();
-        //inputSlot.setTargetBitmap(bitmap);
-        HandOutputSlot outputSlot = (HandOutputSlot) mCVClient.createOutputSlot();
-        mCVClient.process(inputSlot, outputSlot);
-        GestureResultList gestureResultList = outputSlot.getHandList();
-        /*
-        for (GestureResult gestureResult : gestureResultList) {
-            GestureLandmark landmark = gestureResult.getLandmark();
-            List<Float> lmkList =landmark.getLmk();
-        }
-
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recognize);
+        /**
+         * Called when the activity is first created.
          */
-        if (mCVClient != null) {
-            mCVClient.stop();
+        Button button = (Button) findViewById(R.id.button1);
+        view = (ImageView) findViewById(R.id.imageView1);
+        button.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, TAKE_PHOTOS_CODE);
+            }
+        });
+    }
+
+    @SuppressLint("SdCardPath")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        if (requestCode == TAKE_PHOTOS_CODE && resultCode == RESULT_OK) {
+            Bitmap bm = (Bitmap) data.getExtras().get("data");
+            view.setImageBitmap(bm);
+            //initial();
+
+            mCVClient = CVUnit.getGestureLandmarkDetectorClient
+                    (this.getApplicationContext()).addOnConnectionSucceedListener(new OnConnectionSucceedListener() {
+                @Override
+                public void onConnectionSucceed() {
+                    Log.i("TAG", " authorize connect: onConnectionSucceed");
+                }
+            }).addOnConnectionFailedListener(new OnConnectionFailedListener() {
+                @Override
+                public void onConnectionFailed(ConnectionResult connectionResult) {
+                    Log.e("TAG", " authorize connect: onFailure: " + connectionResult.getErrorCode());
+                }
+            });
+            mCVClient.initService(this, new ConnectionCallback() {
+                @Override
+                public void onServiceConnect() {
+                    Log.i("TAG", "initService: onServiceConnect");
+                    int startCode = mCVClient.start();
+                    if(startCode!=0){
+                        System.out.println("there's false startCode");
+                        System.exit(0);
+                    }
+                }
+
+                @Override
+                public void onServiceDisconnect() {
+                    Log.e("TAG", "initService: onServiceDisconnect: ");
+                }
+            });
+
+            //recognize
+            HandInputSlot inputSlot = (HandInputSlot) mCVClient.createInputSlot();
+            inputSlot.setTargetBitmap(bm);
+
+
+            HandOutputSlot outputSlot = (HandOutputSlot) mCVClient.createOutputSlot();
+            mCVClient.process(inputSlot, outputSlot);
+
+            GestureResultList handList = outputSlot.getHandList();
+
+            for (int handOrder = 0; handOrder < handList.getList().size(); ++handOrder) {
+                GestureResult hand = handList.getList().get(handOrder);
+                Rect2Pt boundingBox = hand.getBoundingBox();
+            }
+
+            /*
+            GestureResultList gestureResultList = outputSlot.getHandList();
+
+            for (GestureResult gestureResult : gestureResultList.a) {
+                GestureLandmark landmark = gestureResult.getLandmark();
+                List<Float> lmkList = landmark.getLmk();
+            }
+            */
+            System.out.println(handList.getList().get(0));
+            if (mCVClient != null) {
+                mCVClient.stop();
+                mCVClient.releaseService();
+                mCVClient = null;
+            }
+
         }
-        mCVClient.releaseService();
-        mCVClient = null;
     }
 
 
+    //recognize
+    CVUnitClient mCVClient;
+
+    public void initial() {
+
+
+    }
 }
